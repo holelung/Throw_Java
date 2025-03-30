967d75c1fe0fa947eb350aeb5207615ba999d39b
holelung <junho335276@gmail.com>
2025-03-30 20:16:40 +0900

    Board layout 제작중

--- frontend/TJ_project/src/components/layout/HomePage/HomePage.jsx
+++ frontend/TJ_project/src/components/layout/HomePage/HomePage.jsx
@@ -11,9 +11,11 @@ const HomePage = () => {
         <BasicButton color={"#ffcd64"}>Ask Question</BasicButton>
       </div>
       <div className="board-container">
-        <div className="board">
-          <div className="board-stats"></div>
-          <div className=""></div>
+        <div className="board-data">
+          <div className="board-stats">
+            
+          </div>
+          <div className="board-info"></div>
         </div>
       </div>
     </>
