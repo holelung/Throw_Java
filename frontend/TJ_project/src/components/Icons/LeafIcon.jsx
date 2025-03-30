77e43e9f015d047d2457882eb222af16d75e805c
holelung <junho335276@gmail.com>
2025-03-30 20:22:26 +0900

    ignore

--- frontend/TJ_project/src/components/Icons/LeafIcon.jsx
+++
@@ -1,8 +0,0 @@
-import { faLeaf } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export const LeafIcon = ({size}) => {
-  return (
-    <FontAwesomeIcon icon={faLeaf} size={size} style={{ color: "#63E6BE"}} />
-  );
-}
--- frontend/TJ_project/src/components/layout/Board/Board.jsx
+++
@@ -1,9 +0,0 @@
-const Board = () => { 
-  return (
-    <>
-      <h2>뽀드다 뽀드</h2>
-    </>
-
-  );
-}
-export default Board;
